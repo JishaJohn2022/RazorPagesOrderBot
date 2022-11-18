35d6f5559662a1719564bf1666f946b9c5aca90b
JishaJohn2022 <Jjohn6917@conestogac.on.ca>
2022-11-18 09:39:11 -0500

    Testcases edited

--- OrderBot.tests/OrderBotTest.cs
+++ OrderBot.tests/OrderBotTest.cs
@@ -20,39 +20,39 @@ namespace OrderBot.tests
             Assert.True(sInput.Contains("Welcome"));
         }
         [Fact]
-        public void TestShawarama()
+        public void Testchildcare()
         {
             Session oSession = new Session("12345");
             String sInput = oSession.OnMessage("hello")[0];
-            Assert.True(sInput.ToLower().Contains("shawarama"));
+            Assert.True(sInput.ToLower().Contains("tiny tots"));
         }
         [Fact]
-        public void TestSize()
+        public void TestName()
         {
             Session oSession = new Session("12345");
             String sInput = oSession.OnMessage("hello")[1];
-            Assert.True(sInput.ToLower().Contains("size"));
+            Assert.True(sInput.ToLower().Contains("name"));
         }
         [Fact]
-        public void TestLarge()
+        public void TestDetail()
         {
             Session oSession = new Session("12345");
             oSession.OnMessage("hello");
-            String sInput = oSession.OnMessage("large")[0];
-            Assert.True(sInput.ToLower().Contains("protein"));
-            Assert.True(sInput.ToLower().Contains("large"));
+            String sInput = oSession.OnMessage("john")[0];
+            Assert.True(sInput.ToLower().Contains("detail"));
+            //Assert.True(sInput.ToLower().Contains("large"));
         }
         [Fact]
-        public void TestChicken()
+        public void Teststudent()
         {
             string sPath = DB.GetConnectionString();
             Session oSession = new Session("12345");
             oSession.OnMessage("hello");
-            oSession.OnMessage("large");
-            String sInput = oSession.OnMessage("chicken")[0];
-            Assert.True(sInput.ToLower().Contains("toppings"));
-            Assert.True(sInput.ToLower().Contains("large"));
-            Assert.True(sInput.ToLower().Contains("chicken"));
+            oSession.OnMessage("john");
+            String sInput = oSession.OnMessage("fee")[0];
+            Assert.True(sInput.ToLower().Contains("input"));
+            Assert.True(sInput.ToLower().Contains("student id"));
+         
         }
     }
 }
